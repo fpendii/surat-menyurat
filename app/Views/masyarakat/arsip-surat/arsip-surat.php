702233dfd910bfaebaefe5f8c47c71260ec166f5
amah <norrahmah@mhs.politala.ac.id>
2025-06-28 10:20:06 +0800

    sip

--- app/Views/masyarakat/arsip-surat/arsip-surat.php
+++ app/Views/masyarakat/arsip-surat/arsip-surat.php
@@ -5,16 +5,7 @@
 <div class="x_panel">
     <div class="x_title">
         <h2>Arsip Surat <small>Surat yang Telah Diproses</small></h2>
-        <ul class="nav navbar-right panel_toolbox">
-            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 
-                </div>
-            </li>
-            <li><a class="close-link"><i class="fa fa-close"></i></a></li>
-        </ul>
         <div class="clearfix"></div>
     </div>
 
